5a18e51514356a36d5a77beb038fbaacbd4a03c8
Taylor Bailey <taylor-bailey@live.com>
2019-05-23 16:08:24 -0600

    Irrelevant

--- web/Week03/index.php
+++ web/Week03/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Week 03 - Taylor Bailey</title>
     <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
 </head>
 <body>
     <?php include '../header.php'; ?>
